c63b20b14843903fea67ce467842b2b62f3c9c4a
afernan1000 <andre.fernandes10@gmail.com>
2023-02-24 22:51:55 -0300

    Alterações no Faker

--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-var faker = require('faker');
+import { faker } from '@faker-js/faker';
 // FAKER - MOD. EMAIL, NOME E SOBRENOME
 
 describe('Funcionalidade Login', () => {

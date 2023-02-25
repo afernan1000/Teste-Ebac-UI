ab37c19ef905fa31f75ee65a14ccf9f54ed640e9
afernan1000 <andre.fernandes10@gmail.com>
2023-02-24 22:07:54 -0300

    ALterações para a aula 3

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,34 +2,41 @@
 
 context('Funcionalidade Login', () => {
 
-    it('Deve fazer login com sucesso', () => {
-        /// TESTE
+    beforeEach(() => {
+        // HOOK EXECUTA ANTES DE CADA TESTE
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+
+    afterEach(() => {
+        // HOOK EXECUTA DEPOIS DE CADA TESTE
+        cy.screenshot()
+    });
+
+    it('Deve fazer login com sucesso', () => {
+        // TESTE
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-        /// VALIDAÇÃO
+        // VALIDAÇÃO
         cy.get('.page-title').should('contain', 'Minha conta')
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá,')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
-        /// TESTE
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        // TESTE
         cy.get('#username').type('_ebac@teste.com')
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
-        /// VALIDAÇÃO
+        // VALIDAÇÃO
         cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido')
     })
 
     it('Deve exibir uma mensagem de erro ao inserir senha invalida', () => {
-        /// TESTE
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        // TESTE
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('@teste.com')
         cy.get('.woocommerce-form > .button').click()
-        /// VALIDAÇÃO
+        // VALIDAÇÃO
         cy.get('.woocommerce-error').should('contain', 'está incorreta')        
     })
 
